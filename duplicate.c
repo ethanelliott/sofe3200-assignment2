c33370f018be180bf2bf7dbc707f4ac5e708095d
Ethan Elliott <ethan.elliott@hotmail.ca>
2018-11-19 17:22:57 -0500

    files

---
+++ duplicate.c
@@ -0,0 +1,4 @@
+//
+// Created by Ethan on 19/11/2018.
+//
+
---
+++ part1_test.c
@@ -0,0 +1,8 @@
+// SOFE3200 Systems Programming
+// Assignment 2 - Part 1
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+    printf("Parameters: %d\n", argc);
+    return 0;
+}
